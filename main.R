eb37fb75ebee373446fb8e615ebf4bc2bfad8db4
BobHoutkooper <houtkooper.b@gmail.com>
2016-01-07 14:25:12 +0100

    First steps; making the directory and the files

---
+++ main.R
@@ -0,0 +1,8 @@
+# Loner
+# Bob Houtkooper 	
+# 07-01-2016
+
+# Get the functions
+source(R/leapyear.R)
+source(R/leapyear.R)
+source(R/leapyear.R)
---
+++ numeric.R
@@ -0,0 +1,6 @@
+numeric <- function(year){
+	if(!is.numeric(x)) {
+		stop("x must be of class numeric!")
+	}
+	else
+}
