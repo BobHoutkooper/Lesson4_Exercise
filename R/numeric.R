96460048c34b5cbc72d984676eee47f1d0df437e
BobHoutkooper <houtkooper.b@gmail.com>
2016-01-07 14:38:42 +0100

    Fixed the source function.

--- R/numeric.R
+++ R/numeric.R
@@ -2,5 +2,4 @@ numeric <- function(year){
 	if(!is.numeric(x)) {
 		stop("x must be of class numeric!")
 	}
-	else
 }
--- main.R
+++ main.R
@@ -3,6 +3,6 @@
 # 07-01-2016
 
 # Get the functions
-source(R/leapyear.R)
-source(R/leapyear.R)
-source(R/leapyear.R)
+source('R/leapyear.R')
+source('R/numeric.R')
+source('R/validrange.R')
